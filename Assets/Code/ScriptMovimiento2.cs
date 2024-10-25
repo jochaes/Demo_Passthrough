444a92753e4b4b33f9e5d36d5ada81b14b3122bb
Josué Chaves <d4n11083@estudiantec.cr>
2024-10-25 16:19:42 -0600

    Chore: Docs and file organization

---
+++ Assets/Code/ScriptMovimiento2.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptMovimiento2 : MonoBehaviour
+{
+
+    [SerializeField]
+    private float tiempo = 2f;              //Duracion de la animación
+
+    [SerializeField]
+    private float y_Objetivo = 0.5f;        //Lo que se va a elevar el modelo 
+
+    private bool esVisible = false;         //Nos permite si el modelo esta visible o no
+                                            //
+
+
+    void Start()
+    {
+        //Al iniciar el script, se esconde el modelo, osea baja en y a 0
+        esVisible = false;
+        transform.localPosition = new Vector3(transform.localPosition.x, 0, transform.localPosition.z);
+
+    }
+
+    //Funcion para mover el modelo de su posicion actual a un punto objetivo en y, en un tiempo determinado
+    private IEnumerator Mover(float duracion, float y_Objetivo)
+    {
+        var posicionInicial = transform.localPosition;                                      // Guarda la posición inicial
+        var posicionFinal = new Vector3(posicionInicial.x, y_Objetivo, posicionInicial.z);  // Calcula la posicion final
+        var tiempoTranscurrido = 0f;                                                        // Inicializa un timer
+
+        while (tiempoTranscurrido < duracion) //Mientras que se se transcurre el tiempo 
+        {
+            var t = tiempoTranscurrido / duracion;  // Calcula la interpolacion actual 
+            transform.localPosition = Vector3.Lerp(posicionInicial, posicionFinal, t); //Mueve el modelo
+            tiempoTranscurrido += Time.deltaTime;   // Calcula el tiempo transcurrido
+            yield return null;                      // Espera el siguiente frame
+        }
+
+
+        transform.localPosition = posicionFinal;
+    }
+
+    //Funcion para alternar la visivilidad del modelo
+    public void AlternarVisibilidad()
+    {
+        //Dependiendo de la visibilidad sube o baja el modelo. 
+        if (!esVisible)
+        {
+            StartCoroutine(Mover(tiempo, y_Objetivo));
+            esVisible = true;
+        }
+        else
+        {
+            StartCoroutine(Mover(tiempo, 0));
+            esVisible = false;
+        }
+    }
+
+}
